96b4798a4cabdf2fbf1f1ffe855ce29da051c2b2
murphytodd4 <todd@kingdomprod.com>
2020-02-05 15:35:25 -0600

    Added timeout to arguments. Changed ip argument to just ip and no port

--- Vizio-Always-On.js
+++ Vizio-Always-On.js
@@ -4,10 +4,10 @@ var path = require('path');
 //This script doesn't support a pairing step. The soundbar I have does not require it.
 
 var deviceAddress = process.argv.slice(2)[0];
+var timeout = process.argv.slice(2)[1]; //milliseconds
 
 var filename = path.basename(__filename);
 
-var timeout = 10000; //milliseconds
 
 function setPowerState(state) {
     soundbar.power.currentMode().then((result) => {
@@ -24,16 +24,17 @@ function setPowerState(state) {
 	});
 }
 
-if(deviceAddress){
+if(deviceAddress && timeout){
 	console.log("Running");
-	var soundbar = new vizio(deviceAddress);
+	var soundbar = new vizio(deviceAddress+":9000");
 
 	setInterval(function(){
 		setPowerState(1);
 	}, timeout);
 }
 else{
-	console.log("Usage: node "+filename+" [ipaddress]")
+	console.log("Usage: node "+filename+" [ipaddress] [timeout]")
 	console.log()
 	console.log("ipaddress - can be the ip address or the port can be appended")
+	console.log("timeout - time between attempts in milliseconds")
 }
